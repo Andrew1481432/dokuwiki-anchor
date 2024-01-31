61233efcd5d1afe34901c498d94d3d134ff65827
andrew1481432 <andrew1481432@mail.ru>
2024-01-31 17:36:29 +0300

    add: some code formatting

--- syntax.php
+++ syntax.php
@@ -1,4 +1,5 @@
 <?php
+
 class syntax_plugin_anchor extends DokuWiki_Syntax_Plugin {
 
     CONST PREG_PATTERN = "/^\{\{anchor:(.*):(.*)}}$/ui";
@@ -7,24 +8,25 @@ class syntax_plugin_anchor extends DokuWiki_Syntax_Plugin {
         return 'substition';
     }
 
-	function getPType() {
+    function getPType() {
         return 'normal';
     }
-	function getSort() {
+
+    function getSort() {
         return 167;
     }
 
-	function connectTo($mode) {
-		$this->Lexer->addSpecialPattern('\{\{anchor:[^}]*\}\}', $mode, 'plugin_anchor');
-	}
+    function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('\{\{anchor:[^}]*\}\}', $mode, 'plugin_anchor');
+    }
 
-	function handle($match, $state, $pos, Doku_Handler $handler) {
+    function handle($match, $state, $pos, Doku_Handler $handler) {
         preg_match(self::PREG_PATTERN, $match, $result);
-		return $result;
-	}
+        return $result;
+    }
 
-	function render($format, Doku_Renderer $renderer, $data) {
-        list(/* $rawContent */, $id, $content) = $data; // blame php 5... I want use new syntax list [$rawContent, $id, $content]
-		$renderer->doc .= '<a id="' . $id. '">' . $content . '</a>';
-	}
+    function render($format, Doku_Renderer $renderer, $data) {
+        list(/* $raw */, $id, $content) = $data;
+        $renderer->doc .= '<a id="' . $id. '">' . $content . '</a>';
+    }
 }
